3a181503bec36388a71e8f2f96f454024084e158
Lucas <lucasviniciuslimadepaula@gmail.com>
2022-07-13 12:03:28 -0300

    conclusão de atividade pratica

---
+++ ContaBancaria.js
@@ -0,0 +1,82 @@
+// Orientação a Objetos
+// Este repositório contém a atividade prática do Curso "Orientação a Objetos", que faz parte do Basecamp de Javascript que minstrei pela Digital Innovation One.
+
+// Atividade: Conta Bancária
+// Nesta atividade, vamos testar os conceitos de Orientação a Objetos simulando a criação de diversos tipos de contas bancárias e operações disponíveis em cada uma.
+
+// Crie a classe ContaBancaria, que possui os parâmetros agencia, numero, tipo e saldo;
+// Dentro de ContaBancaria, construa o getter e o setter de saldo;
+// Dentro de ContaBancaria, crie os métodos sacar e depositar;
+// Crie uma classe-filha chamada ContaCorrente que herda todos esses parâmetros e ainda possua o parâmetro cartaoCredito;
+// Ainda em ContaCorrente, construa o getter e o setter de cartaoCredito;
+// Ainda em ContaCorrente, faça com que o tipo seja 'conta corrente' por padrão;
+// Crie uma classe-filha chamada ContaPoupanca que herda todos os parâmetros de ContaBancaria;
+// Crie uma classe-filha chamada ContaUniversitaria que herda todos os parâmetros de ContaBancaria;
+// Faça com que o método saque de ContaUniversitaria apenas seja capaz de sacar valores menores que 500 reais.
+
+class ContaBancaria {
+    constructor(agencia, numero,tipo){
+        this.agencia = agencia
+        this.numero = numero
+        this.tipo = tipo
+        this._saldo = 0
+    }
+    get saldo(){ 
+        return console.log(this._saldo)
+    }
+
+    set saldo(valor){
+        this._saldo = valor;
+    }
+
+    sacar(valor){   
+        if(valor > this._saldo){
+            return "Operação negada, saldo insuficiente"
+        }
+        this._saldo -= valor;
+        return this._saldo
+    }
+
+    depositar(valor){
+        this._saldo += valor;
+        return this._saldo
+    }
+}
+
+class ContaCorrente extends ContaBancaria{
+    constructor(agencia, numero, cartaoCredito){
+        super(agencia, numero)
+        this.tipo = 'corrente';
+        this._cartaoCredito = cartaoCredito;
+    }
+
+    get cartaoCredito(){
+        return this._cartaoCredito;
+    }
+
+    set cartaoCredito(valor){
+        this._cartaoCredito = valor;
+    }
+}
+
+class ContaPoupanca extends ContaBancaria{
+    constructor(agencia, numero){
+        super(agencia, numero)
+        this.tipo = 'poupança';
+    }
+}
+
+class ContaUniversitaria extends ContaBancaria{
+    constructor(agencia, numero){
+        super(agencia, numero)
+        this.tipo = 'universitária';
+    }
+
+    sacar(valor){
+        if(valor > 500){
+            return 'Operação inválida, somente saque de valores abaixo de R$500 são permitidos.'
+        }
+        this._saldo -= valor;
+        return this._saldo
+    }
+}
